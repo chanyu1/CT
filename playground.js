dab0404b05d5516da172dbd07111efe16bac3793
chanyu1 <haneul1ij@gmail.com>
2021-10-14 21:36:01 +0900

    study js

--- playground.js
+++ playground.js
@@ -1,19 +1,48 @@
-const mike = {
-  name: "Mike",
+const f1 = () => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res("1");
+    }, 1000);
+  });
 };
 
-const tom = {
-  name: "Tom",
+const f2 = (message) => {
+  // console.log(message);
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res("2");
+    }, 2000);
+  });
 };
 
-function showThisName() {
-  console.log(this.name);
+const f3 = (message) => {
+  // console.log(message);
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res("3");
+    }, 3000);
+  });
+};
+
+// f1()
+//   .then((res) => f2(res))
+//   .then((res) => f3(res))
+//   .then((res) => console.log(res))
+//   .catch(console.log);
+
+async function order() {
+  try {
+    // const result1 = await f1();
+    // const result2 = await f2(result1);
+    // const result3 = await f3(result2);
+    // console.log(result3);
+
+    const result = await Promise.all([f1(), f2(), f3()]);
+    console.log(result);
+  } catch (e) {
+    console.log(e);
+  }
+  console.log("exit");
 }
 
-function update(birthYear, occupation) {
-  this.birthYear = birthYear;
-  this.occupation = occupation;
-}
-
-update.apply(mike, [1999, "singer"]);
-console.log(mike);
+order();
